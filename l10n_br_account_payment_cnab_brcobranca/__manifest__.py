cc5519b7feb8db90c030f08118d18730b51972e2
Magno Costa <magno.costa@akretion.com.br>
2020-06-17 12:31:12 -0300

    [12.0][MIG][REF] Included data for CNAB Sequence and change version of module.

--- l10n_br_account_payment_cnab_brcobranca/__manifest__.py
+++ l10n_br_account_payment_cnab_brcobranca/__manifest__.py
@@ -7,16 +7,17 @@
     'name': 'L10n Br Account Payment CNAB Brcobranca',
     'description': """
         Imprime boletos usando a Gem brcobranca do Boletosimples""",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Akretion',
     'website': 'www.akretion.com',
     'depends': [
-        'l10n_br_account_banking_payment_cnab'
+        'l10n_br_account_payment_cobranca',
     ],
     'data': [
         'views/payment_mode.xml',
         'views/l10n_br_cnab_retorno_view.xml',
+        'data/cnab_data.xml',
     ],
     'demo': [
     ],
