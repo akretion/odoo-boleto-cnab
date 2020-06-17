dec0a1e18a9123a1e2e9e8cb5a2c625720e64773
Magno Costa <magno.costa@akretion.com.br>
2020-06-17 12:50:41 -0300

    [12.0][MIG][REF][WIP] Join modules Boleto and CNAB, delete module.

--- l10n_br_account_payment_cnab_brcobranca/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 KMEE - Luiz Felipe do Divino Costa <luiz.divino@kmee.com.br>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import l10n_br_cnab
-from . import payment_mode
-from . import account_payment_order
